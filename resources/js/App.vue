e14b89beb239f7e60fd242dcf6953e44f8921f43
RC0D3 <rc0d3m4ax@gmail.com>
2023-10-23 17:16:04 -0300

    added: vuejs and tailwindcss dependency and simple hello

---
+++ resources/js/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1 class="text-2xl text-red-500"> Hello, Vuejs with Laravel and Tailwind css</h1>
+  </template>
+  <script>
+  export default {
+    setup() {
+  
+     }
+  }
+  </script>
