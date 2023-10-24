1ad1cf53b7f2daee91ab81036e1fbfc0d576b6c3
RC0D3 <rc0d3m4ax@gmail.com>
2023-10-24 18:05:04 -0300

    update: added 2 list to drag items

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,8 +1,9 @@
 <template>
   <h1 class="text-2xl text-red-500"> Hello, Vuejs with Laravel and Tailwind css</h1>
   <div class="min-h-screen w-screen bg-gray-200 flex items-center justify-center">
-    <ul class="w-full max-w-md">
-      <draggable :list="users" item-key="id" tag="ul" class="w-full max-w-md" :animation="200" ghost-class="moving-card" filter=".action-button">
+    <div class="w-full max-w-md text-center">
+      <p class="mb-2 text-gray-700 font-semibold font-sans tracking-wide">List 1</p>
+      <draggable group="all-users" :list="users" item-key="id" tag="ul" class="w-full max-w-md" :animation="200" ghost-class="moving-card" filter=".action-button">
         <template #item="{ element }">
           <UserCard :user="element" :key="element.id" @on-edit="onEdit" @on-delete="onDelete">
           </UserCard>
@@ -19,12 +20,32 @@
           </li>
         </template>
       </draggable>
-    </ul>
+    </div>
+    <div class="w-full max-w-md ml-12 text-center">
+      <p class="mb-2 text-gray-700 font-semibold font-sans tracking-wide">List 2</p>
+      <draggable group="all-users" :list="newUsers" item-key="id" tag="ul" class="w-full max-w-md" :animation="200" ghost-class="moving-card" filter=".action-button">
+        <template #item="{ element }">
+          <UserCard :user="element" :key="element.id" @on-edit="onEdit" @on-delete="onDelete">
+          </UserCard>
+        </template>
+
+        <template #footer>
+          <li class="p-1 mb-3 flex justify-between items-center bg-white shadow rounded-lg">
+            <div class="flex items-center w-full justify-center">
+              <button aria-label="Delete user" class="flex action-button p-1 focus:outline-none focus:shadow-outline text-gray-400 hover:text-gray-700" @click="onAdd">
+                <p class="font-semibold font-sans tracking-wide">Adicionar novo usuário</p>
+                <vue-feather type="plus" class="relative top-1 ml-1"></vue-feather>
+              </button>
+            </div>
+          </li>
+        </template>
+      </draggable>
+    </div>
   </div>
 </template>
 <script>
 import draggable from 'vuedraggable';
-import UserCard from './UserCard.vue';
+import UserCard from './components/UserCard.vue';
 let id = 1;
 
 export default {
@@ -60,6 +81,8 @@ export default {
           name: "Senior Saez",
           avatar: "https://robohash.org/Senior"
         }
+      ],
+      newUsers: [
       ]
     }
   },
