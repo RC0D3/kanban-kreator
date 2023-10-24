288956a784145d00cbf6ec75f5b80093c6577460
RC0D3 <rc0d3m4ax@gmail.com>
2023-10-24 17:09:07 -0300

    added: basic list operations

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,51 +2,61 @@
   <h1 class="text-2xl text-red-500"> Hello, Vuejs with Laravel and Tailwind css</h1>
   <div class="min-h-screen w-screen bg-gray-200 flex items-center justify-center">
     <ul class="w-full max-w-md">
-      <li v-for="user in users" :key="user.id" class="p-4 mb-3 flex justify-between items-center bg-white shadow rounded-lg cursor-move">
-        <div class="flex items-center w-full">
-          <img class="w-10 h-10 rounded-full" :src="user.avatar" :alt="user.name">
-          <p class="ml-2 text-gray-700 font-semibold font-sans tracking-wide">{{ user.name }}</p>
-          <div class="flex flex-1 justify-end">
-            <button aria-label="Edit user" class="p-1 focus:outline-none focus:shadow-outline text-teal-500 hover:text-teal-600" @click="onEdit(user)">
-              <vue-feather type="edit"></vue-feather>
-            </button>
-            <button aria-label="Delete user" class="p-1 focus:outline-none focus:shadow-outline text-red-500 hover:text-red-600" @click="onDelete(user)">
-              <vue-feather type="star" stroke="red"></vue-feather>
-            </button>
-          </div>
-        </div>
-      </li>
+      <draggable :list="users" item-key="id" tag="ul" class="w-full max-w-md" :animation="200" ghost-class="moving-card" filter=".action-button">
+        <template #item="{ element }">
+          <UserCard :user="element" :key="element.id" @on-edit="onEdit" @on-delete="onDelete">
+          </UserCard>
+        </template>
+
+        <template #footer>
+          <li class="p-1 mb-3 flex justify-between items-center bg-white shadow rounded-lg">
+            <div class="flex items-center w-full justify-center">
+              <button aria-label="Delete user" class="flex action-button p-1 focus:outline-none focus:shadow-outline text-gray-400 hover:text-gray-700" @click="onAdd">
+                <p class="font-semibold font-sans tracking-wide">Adicionar novo usuário</p>
+                <vue-feather type="plus" class="relative top-1 ml-1"></vue-feather>
+              </button>
+            </div>
+          </li>
+        </template>
+      </draggable>
     </ul>
   </div>
 </template>
 <script>
+import draggable from 'vuedraggable';
+import UserCard from './UserCard.vue';
+let id = 1;
 
 export default {
+  components: {
+    draggable,
+    UserCard,
+  },
   data() {
     return {
       users: [
         {
-          id: 1,
+          id: id++,
           name: "Adrian Schubert",
           avatar: "https://robohash.org/Adrian%20Schubert"
         },
         {
-          id: 2,
+          id: id++,
           name: "Violet Gates",
           avatar: "https://robohash.org/Violet"
         },
         {
-          id: 3,
+          id: id++,
           name: "Steve Jobs",
           avatar: "https://robohash.org/Steve%20Jobs"
         },
         {
-          id: 4,
+          id: id++,
           name: "Yassine Smith",
           avatar: "https://robohash.org/Yassine"
         },
         {
-          id: 5,
+          id: id++,
           name: "Senior Saez",
           avatar: "https://robohash.org/Senior"
         }
@@ -58,8 +68,17 @@ export default {
       alert(`Editing ${user.name}`);
     },
     onDelete(user) {
-      alert(`Deleting ${user.name}`);
+      this.users = this.users.filter((u) => u.id !== user.id);
+    },
+    onAdd() {
+      this.users.push({ id: id++, name: "NOVO USER", avatar: "https://robohash.org/Novo" })
     }
   }
 }
 </script>
+
+<style scoped>
+.moving-card {
+  @apply opacity-50 bg-gray-100 border border-blue-500;
+}
+</style>
---
+++ resources/js/UserCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <li class="p-4 mb-3 flex justify-between items-center bg-white shadow rounded-lg cursor-move">
+        <div class="flex items-center w-full">
+            <img class="w-10 h-10 rounded-full" :src="user.avatar" :alt="user.name">
+            <p class="ml-2 text-gray-700 font-semibold font-sans tracking-wide">{{ user.name }}</p>
+            <div class="flex flex-1 justify-end">
+                <button aria-label="Edit user" class="action-button p-1 focus:outline-none focus:shadow-outline text-teal-500 hover:text-teal-600" @click="$emit('on-edit', user)">
+                    <vue-feather type="edit"></vue-feather>
+                </button>
+                <button aria-label="Delete user" class="action-button p-1 focus:outline-none focus:shadow-outline text-red-500 hover:text-red-600" @click="$emit('on-delete', user)">
+                    <vue-feather type="trash-2"></vue-feather>
+                </button>
+            </div>
+        </div>
+    </li>
+</template>
+
+<script>
+
+export default {
+    props: {
+        user: {
+            type: Object,
+            default: () => ({}),
+        }
+    }
+};
+</script>
